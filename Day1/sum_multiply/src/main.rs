d7509e85a2deec3885cc3de561b614e557fd2e9e
dylankirby <dylan@pagefreezer.com>
2021-01-27 08:22:45 -0800

    start refactor of day1

--- Day1/sum_multiply/src/main.rs
+++ Day1/sum_multiply/src/main.rs
@@ -1,31 +1,43 @@
 use std::fs;
-use std::io::ErrorKind;
+
+extern crate argparse;
+
+use argparse::{ArgumentParser, Store};
+use itertools::Itertools;
 
 const FILENAME: &str = "data.txt";
+const NUM_VALUES: i64 = 2;
 
-fn main() {
-    let contents = fs::read_to_string(FILENAME)
-        .expect("Something went wrong reading the file");
-
-    let numbers: Vec<i32> = contents
-        .split_whitespace()
-        .map(|s| s.parse().expect("parse error"))
-        .collect();
-    let (num1, num2, num3) = find_2020_sum(numbers);
-
-    let multiplied_value = num1 * num2 * num3;
-    println!("{}", multiplied_value);
+enum TwoOrThreeTuple<'a> {
+    TwoTuple(&'a (i64, i64)),
+    ThreeTuple(&'a (i64, i64, i64)),
 }
 
-fn find_2020_sum(values: Vec<i32>) -> (i32, i32, i32){
-    for value in &values {
-        let pair_value: i32 = 2020 - value;
-        for inner_value in &values {
-            let second_pair_value: i32 = pair_value - inner_value;
-            if values.contains(&second_pair_value) {
-                return (*value, *inner_value, second_pair_value)
-            }
-        }
+fn main() -> anyhow::Result<()> {
+    let contents = fs::read_to_string(FILENAME)?;
+
+    let numbers: Vec<i64> = contents
+        .split_whitespace() // splits string contents on whitespce, returns split
+        .map(str::parse::<i64>) // maps str parse for i32 to all values
+        .collect::<Result<Vec<_>, _>>()?; // collects split into in memory vector, return Err if occurs thanks to ?
+
+    let found_values = find_x_values_that_sum_to_target(NUM_VALUES, numbers, 2020);
+    dbg!(found_values);
+
+    // let multiplied_value = num1 * num2;
+    // println!("{}", multiplied_value);
+    Ok(())
+}
+
+fn find_x_values_that_sum_to_target(num_values: i64, values: Vec<i64>, target: i64) -> Option<TwoOrThreeTuple<'static>> {
+    if num_values < 2 || num_values > 3 {
+        panic!("Only 2/3 values supported for processing")
     }
-    (0, 0, 0)
+    let as_tuples = values.into_iter().tuple_combinations();
+
+    if num_values == 2 {
+        TwoOrThreeTuple::TwoTuple(as_tuples.find(|(a, b)| a + b == target))
+    } else {
+        TwoOrThreeTuple::ThreeTuple(as_tuples.find(|(a, b, c)| a + b + c == 2020))
+    }
 }
