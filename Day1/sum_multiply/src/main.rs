447e39785744aeafa89cd4d575bdddae2fe152a0
Dylan Kirby <dylankirby@Dylans-Mac-mini.local>
2021-01-27 20:31:40 -0800

    v2 of day 1 complete

--- Day1/sum_multiply/src/main.rs
+++ Day1/sum_multiply/src/main.rs
@@ -2,15 +2,14 @@ use std::fs;
 
 extern crate argparse;
 
-use argparse::{ArgumentParser, Store};
 use itertools::Itertools;
 
 const FILENAME: &str = "data.txt";
 const NUM_VALUES: i64 = 2;
 
-enum TwoOrThreeTuple<'a> {
-    TwoTuple(&'a (i64, i64)),
-    ThreeTuple(&'a (i64, i64, i64)),
+enum ResultTuple {
+    TwoTuple(Option<(i64, i64)>),
+    ThreeTuple(Option<(i64, i64, i64)>),
 }
 
 fn main() -> anyhow::Result<()> {
@@ -21,23 +20,33 @@ fn main() -> anyhow::Result<()> {
         .map(str::parse::<i64>) // maps str parse for i32 to all values
         .collect::<Result<Vec<_>, _>>()?; // collects split into in memory vector, return Err if occurs thanks to ?
 
-    let found_values = find_x_values_that_sum_to_target(NUM_VALUES, numbers, 2020);
-    dbg!(found_values);
+    match find_x_values_that_sum_to_target(NUM_VALUES, numbers, 2020) { //TODO: Remove duplication here
+        ResultTuple::TwoTuple(value) => {
+            let numbers = value.unwrap();
+            let answer = numbers.0 * numbers.1;
+            println!("Numbers are {} and {}, and the answer is {}", numbers.0, numbers.1, answer);
+        }
+        ResultTuple::ThreeTuple(value) => {
+            let numbers = value.unwrap();
+            let answer =  numbers.0 * numbers.1 * numbers.2;
+            println!("Numbers are {}, {}, and {}, and the answer is {}", numbers.0, numbers.1, numbers.2, answer);
+        }
+    }
 
     // let multiplied_value = num1 * num2;
     // println!("{}", multiplied_value);
     Ok(())
 }
 
-fn find_x_values_that_sum_to_target(num_values: i64, values: Vec<i64>, target: i64) -> Option<TwoOrThreeTuple<'static>> {
+fn find_x_values_that_sum_to_target(num_values: i64, values: Vec<i64>, target: i64) -> ResultTuple{
     if num_values < 2 || num_values > 3 {
         panic!("Only 2/3 values supported for processing")
     }
-    let as_tuples = values.into_iter().tuple_combinations();
 
     if num_values == 2 {
-        TwoOrThreeTuple::TwoTuple(as_tuples.find(|(a, b)| a + b == target))
+        return ResultTuple::TwoTuple(values.into_iter().tuple_combinations().find(|(a, b)| a + b == target));
     } else {
-        TwoOrThreeTuple::ThreeTuple(as_tuples.find(|(a, b, c)| a + b + c == 2020))
+        return ResultTuple::ThreeTuple(values.into_iter().tuple_combinations().find(|(a, b, c)| a + b + c == 2020));
     }
+
 }
